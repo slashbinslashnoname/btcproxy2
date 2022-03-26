8e1df1ab4adac09d1bc57bef07cbb3605c736c21
Slashbinslashnoname <slashbinslashnoname@gmail.com>
2022-03-26 14:58:54 +0100

    run edit

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it --restart=always -p 3340:3333 stratum-mining-proxy -o europe.soloblocks.io -p 3334 -sh 0.0.0.0 -sp 3333 -cu bc1qkza6rwt8hd6z5xz8rz4rl284fn57l4lly8p0l8
+docker run -it -d --restart=always -p 3340:3333 --name stratum-mining-proxy stratum-mining-proxy -o europe.soloblocks.io -p 3334 -sh 0.0.0.0 -sp 3333 -cu bc1qkza6rwt8hd6z5xz8rz4rl284fn57l4lly8p0l8
